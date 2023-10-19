8cef6dcb7230da23d8d2b7271f947077e453bc38
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-19 22:15:49 +0200

    version 2. With guessing limit

--- GuessGame/main.cpp
+++ GuessGame/main.cpp
@@ -2,28 +2,78 @@
 
 using namespace std;
 
-void showYourSecret(int);
+void showYourSecretNoLimit(int);
+void showYourSecretLimit(int);
 
 int main()
 {
     int userSecret;
+    int userLimitChose;
+
     cout << "What is your secret number? ";
     cin >> userSecret;
-    showYourSecret(userSecret);
+    cout << "Do you want to play with guess limit or without?" << endl;
+    cout << "0-without limit, 1-with limit" << endl;
+    cin >> userLimitChose;
+
+    switch(userLimitChose) {
+    case 0:
+           showYourSecretNoLimit(userSecret);
+           break;
+    case 1:
+           showYourSecretLimit(userSecret);
+           break;
+    default:
+        cout << "Wrong choice";
+        return -1;
+    }
+
     return 0;
 }
 
-void showYourSecret(int secretNumber) {
+void showYourSecretNoLimit(int secretNumber) {
     int userInput;
     cout << "What a number: ";
     cin >> userInput;
+
     while(userInput > 0) {
        if(userInput == secretNumber) {
         cout << "Great your know the secret. The number is: " << userInput << endl;
         return;
        }
-       cout << "Try again" << endl;
-       cout << "What a number: ";
-       cin >> userInput;
+
+    cout << "Try again" << endl;
+    cout << "What a number: ";
+    cin >> userInput;
+   }
+}
+
+void showYourSecretLimit(int secretNumber) {
+    int limit = 10;
+    int userTries = 0;
+    cout << "You have only 10 tries" << endl;
+    int userInput;
+    cout << "What a number: ";
+    cin >> userInput;
+
+    while(userInput > 0) {
+
+        if(userTries == limit) {
+          cout<<"Sorry!You lose the game" << endl;
+          cout<<"You do not have more tries" << endl;
+          cout<<"The secretNumber was " << secretNumber << endl;
+          return;
+        }
+
+        if(userInput == secretNumber) {
+        cout << "Great your know the secret. The number is: " << userInput << endl;
+        return;
+       }
+
+    userTries++;
+    cout << "Try again" << endl;
+    cout << "What a number: ";
+    cin >> userInput;
     }
 }
+
