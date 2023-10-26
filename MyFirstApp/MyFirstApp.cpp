5871d7003927f8bc2d64b29b61fd033b9a570716
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-26 22:58:49 +0200

    first test task

---
+++ MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    char c1, c2, c3, c4, c5;
+    std::cout << "Enter 5 variables: ";
+    std::cin >> c1 >> c2 >> c3 >> c4 >> c5;
+    std::cout << "ASCII message: " << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " 
+        << int(c5) << std::endl;
+    std::cout << "Human read message: " << char(int(c1)) << " " << char(int(c2)) << " " << char(int(c3)) 
+        << " " << char(int(c4)) << " "
+        << char(int(c5)) << std::endl;
+    system("pause>0");
+}
+
+
