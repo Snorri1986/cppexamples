e607d7c9c6cb7bce6255fb0ccc2a237233860b4b
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-01-02 20:47:30 +0100

    =Multidynamicsarray

---
+++ PointerToPointers/PointerToPointers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    int rows, cols;
+    std::cout << "rows,cols";
+    std::cin >> rows >> cols;
+
+    int** table = new int*[rows];
+    for (int i = 0;i < rows;i++) {
+        table[i] = new int[cols];
+    }
+
+    table[1][2] = 88;
+
+    //dealocation memory
+    for (int i = 0;i < rows;i++) {
+        delete[] table[i];
+    }
+
+    delete[] table;
+    table = NULL;
+
+    system("pause>0");
+}
+
+
