d5c6f47d86b75f2f5b751144e652846d59706974
Denys Shabelnyk <dionisiy1986@gmail.com>
2026-05-12 21:32:26 +0200

    #1 - main and header file

---
+++ DateC++/Date.h
@@ -0,0 +1,39 @@
+#ifndef GITHUBREPO_DATE_H
+#define GITHUBREPO_DATE_H
+
+class Date
+{
+    public:
+
+    Date()
+    {
+        setMonth(1);
+        setDay(1);
+        setYear(1970);
+    };
+
+    Date(const int m, const int d, const int y)
+    {
+        setMonth(m);
+        setDay(d);
+        setYear(y);
+    }
+
+            int getMonth();
+            int getDay();
+            int getYear();
+
+            void setMonth(int month);
+            void setDay(int day);
+            void setYear(int year);
+
+            // task related functions
+            void init(int m, int d, int y);
+            void init();
+            void print();
+
+    private:
+            int month, day, year;
+};
+
+#endif //GITHUBREPO_DATE_H
---
+++ DateC++/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    // temporary code
+    std::cout << "Hello World!" << std::endl;
+}
+
